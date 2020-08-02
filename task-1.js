47a771e8ab8988234ca0be8dac685dd08926e353
Koval <dmytrokovalskyi@yahoo.com>
2020-08-02 23:02:43 +0300

    1 task module 7

---
+++ task-1.js
@@ -0,0 +1,26 @@
+
+// const selectedById = document.querySelector('#categories');
+
+// console.log(selectedById);
+
+// const title = document.querySelector('.title');
+// title.textContent = "dkdkdkd";
+// console.log (title)
+
+// const title = document.querySelector(".title");
+// console.log(title.classList);
+// title.classList.add('newClass-js');
+// title.style.backgroundColor = "red";
+// console.log (title.classList)
+// console.log(title.classList.contains ('green'))
+//////-------1 task -----------///////////////
+// const categories = document.querySelector('#categories');
+
+// console.log(categories.children);
+
+
+// const listOfCategories = document.querySelectorAll('#categories .item');
+// listOfCategories.forEach((elem) => console.log(elem.querySelector('h2'), elem.querySelectorAll("li").length));
+ 
+
+
