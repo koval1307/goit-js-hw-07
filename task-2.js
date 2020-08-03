a1d8b77d8950b65eb4a16b93a8c0247bb8fdb945
Koval <dmytrokovalskyi@yahoo.com>
2020-08-03 16:51:40 +0300

    добавил результат в массив 8 и 9

--- task-2.js
+++ task-2.js
@@ -1,15 +1,51 @@
-const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
-];
+// const ingredients = [
+//     'Картошка',
+//     'Грибы',
+//     'Чеснок',
+//     'Помидоры',
+//     'Зелень',
+//     'Приправы',
+// ];
 
-const listOfIngredients = ingredients.map((ingredient) => {
-    const ingredientsName = document.createElement('li');
-    ingredientsName.textContent = ingredient;
-    return (ingredientsName);
-})
-document.querySelector("#ingredients").append(...listOfIngredients);
+// const listOfIngredients = ingredients.map((ingredient) => {
+//     const ingredientsName = document.createElement('li');
+//     ingredientsName.textContent = ingredient;
+//     return (ingredientsName);
+// })
+// document.querySelector("#ingredients").append(...listOfIngredients);
+
+
+////-------==========-practice----==========-------//////
+// const box = document.querySelector('div');
+// console.log(box)
+// const squareOne = document.createElement("p");
+// squareOne.textContent = '1';
+// const squareTwo = document.createElement("p");
+// squareTwo.textContent = '2';
+// box.append(squareOne);
+// box.append(squareTwo)
+
+
+
+// squareOne.style.backgroundColor = 'red';
+// squareTwo.style.backgroundColor = "green";
+// squareOne.style.width = '60px'
+// squareTwo.style.width = '60px';
+
+// const switchNum = function () {
+
+//     if (squareOne.textContent === '1') {
+//         squareOne.textContent = "2"
+//         squareTwo.textContent = '1'
+//     }
+//     else if (squareOne.textContent === '2')
+//     {
+//         squareTwo.textContent = '2'
+//     squareOne.textContent = '1'}
+ 
+    
+// }
+// squareTwo.addEventListener('click', switchNum);
+// squareOne.addEventListener('click', switchNum);
+
+///////==================================///////////////////
